b04120fc056c973954182b0c1c869e11f19a4d04
Blake Vieyra <blake.vieyra@gmail.com>
2023-11-01 12:59:43 -0700

    adding objecties 1 and 2

---
+++ labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+  public static void main(String[] args) {
+    String name = "Blake Vieyra";
+    int age = 29;
+    String state = "California";
+    System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+  }
+}
